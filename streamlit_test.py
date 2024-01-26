7b944eb9a8e4299c8f34f9f7d87f63a415ea93ea
tompritsky <tpritsky@gmail.com>
2024-01-26 14:06:15 -0800

    Updated title

--- streamlit_test.py
+++ streamlit_test.py
@@ -29,16 +29,23 @@ dialogflow_code = """
             agent-id="94d1a3fb-2068-439e-8a1c-293afb924030"
             language-code="en"
             max-query-length="-1">
-            <df-messenger-chat-bubble chat-title="cqa-test"></df-messenger-chat-bubble>
+            <df-messenger-chat-bubble chat-title="cqa2-test"></df-messenger-chat-bubble>
         </df-messenger>
     </div>
 """
 
-st.title("Streamlit App")
+st.title("Vertex AI chatbot demo")
+
+# Display Dialogflow Messenger in the second column
 
 # Create Streamlit columns
 col1, col2 = st.columns([1, 1])
-
-# Display Dialogflow Messenger in the second column
+with col1:
+    uploaded_files = st.file_uploader("Choose a PDF file", accept_multiple_files=True)
 with col2:
     st.components.v1.html(dialogflow_code, height=650)
+
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
